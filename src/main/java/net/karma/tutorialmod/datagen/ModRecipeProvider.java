5c13500f63e88ab1c3b0bdf96202d1f16342466b
CorruptedKarma <karmii044@gmail.com>
2024-05-13 21:46:31 -0400

    Im going through changes :v

--- src/main/java/net/karma/tutorialmod/datagen/ModRecipeProvider.java
+++ src/main/java/net/karma/tutorialmod/datagen/ModRecipeProvider.java
@@ -6,7 +6,9 @@ import net.fabricmc.fabric.api.datagen.v1.provider.FabricRecipeProvider;
 import net.karma.tutorialmod.block.ModBlock;
 import net.karma.tutorialmod.item.ModItems;
 import net.minecraft.data.server.recipe.RecipeJsonProvider;
+import net.minecraft.data.server.recipe.ShapedRecipeJsonBuilder;
 import net.minecraft.item.ItemConvertible;
+import net.minecraft.item.Items;
 import net.minecraft.recipe.book.RecipeCategory;
 
 import java.util.List;
@@ -31,8 +33,25 @@ public class ModRecipeProvider extends FabricRecipeProvider {
                 6.5f, 400,"void_crystal");
 
         offerReversibleCompactingRecipes(exporter, RecipeCategory.BUILDING_BLOCKS, ModItems.Void_Crystal, RecipeCategory.DECORATIONS,
-                ModBlock.Void_Ore);
+                ModBlock.VoidCrystal_BLOCK);
 
-
+        ShapedRecipeJsonBuilder.create(RecipeCategory.MISC, ModItems.VOID_INGOT, 1)
+                .pattern("SSS")
+                .pattern("SRS")
+                .pattern("SSS")
+                .input('S', ModItems.Void_infused_Pearl)
+                .input('R', ModItems.Iron_Cassing)
+                .criterion(hasItem(ModItems.Iron_Cassing), conditionsFromItem(ModItems.Iron_Cassing))
+                .criterion(hasItem(ModItems.Void_infused_Pearl), conditionsFromItem(ModItems.Void_infused_Pearl))
+                .offerTo(exporter);
+        ShapedRecipeJsonBuilder.create(RecipeCategory.MISC, ModItems.Void_Amethyst, 2)
+                .pattern(" S ")
+                .pattern("SRS")
+                .pattern(" S ")
+                .input('S', Items.AMETHYST_SHARD)
+                .input('R', ModItems.Void_Crystal)
+                .criterion(hasItem(Items.AMETHYST_SHARD), conditionsFromItem(Items.AMETHYST_SHARD))
+                .criterion(hasItem(ModItems.Void_Crystal), conditionsFromItem(ModItems.Void_Crystal))
+                .offerTo(exporter);
     }
 }
